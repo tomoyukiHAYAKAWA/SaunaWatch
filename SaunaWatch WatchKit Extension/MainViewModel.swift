f2845e7fbfff7a94a16cb60072b9afec7554a748
tomoyukiHAYAKAWA <tomotarosu13@gmail.com>
2022-09-14 01:05:14 +0900

    add:タイマーの開始、停止、リセット制御の追加

--- SaunaWatch WatchKit Extension/MainViewModel.swift
+++ SaunaWatch WatchKit Extension/MainViewModel.swift
@@ -11,6 +11,25 @@ final class MainViewModel: ObservableObject {
     private var cancellable: AnyCancellable?
 
     init() {
+        startTimer()
+    }
+
+    /// サークルの進行を開始・停止する
+    func tappedStartButton() {
+        isProgression ? cancellable?.cancel() : startTimer()
+        isProgression.toggle()
+    }
+
+    /// サークルの進行をリセットする
+    func tappedRepeatButton() {
+        timerCount = 0
+        progressionValue = 0.0
+        timerString = "00:00"
+    }
+
+    // MARK: - Private
+
+    private func startTimer() {
         cancellable = Timer.publish(every: 1, on: .main, in: .default)
             .autoconnect()
             .sink { [weak self] _ in
@@ -18,23 +37,15 @@ final class MainViewModel: ObservableObject {
             }
     }
 
-    /// サークルの進行を開始・停止する
-    func tappedStartButton() {
-        isProgression.toggle()
-    }
-
-    /// サークルの進行をリセットする
-    func tappedRepeatButton() {
-    }
-
-    // MARK: - Private
     private func countUp() {
         if timerCount > 720 { timerCount = 0 }
         timerCount = timerCount + 1
+        // 円を進める値
         progressionValue = CGFloat(timerCount) / 720.0
-        // 00:00のフォーマットで表示する
+        // 00:00の形式でフォーマット
         let m = (timerCount % 3600) / 60
         let s = (timerCount % 60)
+        // 経過時間
         timerString = String(String(format: "%02d:%02d", m, s))
     }
 }
